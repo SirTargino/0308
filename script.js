6caba1025df0a3e051eff3bad8b417dd96548b97
João Targino <joaoetargino@gmail.com>
2023-08-03 18:06:56 -0300

    adicionando o codigo

---
+++ script.js
@@ -0,0 +1,23 @@
+let nomes = ["joão","anderson","manu"]
+
+console.log(nomes)//Mostrar no console
+
+nomes.push("luna")//Adiciona na última posição
+
+console.log(nomes)//Mostrar no console
+
+nomes.unshift("simon")//Adicionar na posição 0
+
+console.log(nomes)//Mostrar no console
+
+nomes.pop()//Remove o último elemento
+
+console.log(nomes)//Mostrar no console
+
+nomes.pop()//Remove o último elemento
+
+console.log(nomes)//Mostrar no console
+
+nomes.push("luna")//Adiciona na última posição
+
+console.log(nomes)//Mostrar no console
